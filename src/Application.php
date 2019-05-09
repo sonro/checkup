dfe8f7f000ece22f4c39b6acad1d797eb6e10a10
sonro <sonro@gmx.com>
2019-05-09 17:56:06 +0100

    save badUrls to file

--- src/Application.php
+++ src/Application.php
@@ -24,8 +24,44 @@ class Application
         $urlList = UrlListBuilder::buildFromFile($urlFile);
 
         $badUrls = $this->container->getUrlTester()->testList($urlList);
+        if (empty($badUrls)) {
+            return;
+        }
+
+        if ($this->isBadUrlNew($badUrls)) {
+            $this->saveBadUrls($badUrls);
+            $this->sendEmails($badUrls);
+        }
 
         var_dump($badUrls);
+    }
+
+    private function sendEmails(array $badUrls)
+    {
+        echo "Sending emails\n";
+    }
+
+    private function saveBadUrls(array $badUrls)
+    {
+        $urlFile = $this->container->getConfig()->getBadSitesFile();
+        $output = implode("\n", $badUrls);
+        file_put_contents($urlFile, $output);
+    }
+
+    private function isBadUrlNew(array $badUrls): bool
+    {
+        $urlFile = $this->container->getConfig()->getBadSitesFile();
+        if (!file_exists($urlFile)) {
+            return true;
+        }
+        $oldUrls = UrlListBuilder::buildFromFile($urlFile);
+        foreach ($badUrls as $newUrl) {
+            if (array_search($newUrl, $oldUrls, true) === false) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private function testInternetAccess(): bool
@@ -34,7 +70,7 @@ class Application
         $result = $this->container->getUrlTester()->testOne($testUrl, false);
         if (!$result) {
             $this->container->getLogger()
-                ->warning('Unreachable test URL', [$testUrl]);
+                ->error('Unreachable test URL', [$testUrl]);
         }
 
         return $result;
