9c823873ee7d5686b042bd07b859c6f87d3934f3
Christopher Morton <sonro@gmx.com>
2022-01-04 14:09:22 +0000

    Setup test suites

---
+++ tests/Itegration/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sonro\Checkup\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function test_integration_tests_running()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sonro\Checkup\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function test_unit_tests_running()
+    {
+        $this->assertTrue(true);
+    }
+}
