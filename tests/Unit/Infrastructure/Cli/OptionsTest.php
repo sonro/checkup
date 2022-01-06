932a7602b8b4cddadc29880e6139fa345c4949a6
Christopher Morton <sonro@gmx.com>
2022-01-06 18:07:36 +0000

    Add more Options tests

--- tests/Unit/Infrastructure/Cli/OptionsTest.php
+++ tests/Unit/Infrastructure/Cli/OptionsTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Sonro\Checkup\Tests\Unit\Infrastructure\Cli;
 
 use PHPUnit\Framework\TestCase;
+use Sonro\Checkup\Infrastructure\Cli\Arguments;
+use Sonro\Checkup\Infrastructure\Cli\EnvironmentError;
 use Sonro\Checkup\Infrastructure\Cli\Options;
 
 class OptionsTest extends TestCase
@@ -32,16 +34,81 @@ class OptionsTest extends TestCase
     public function test_default(): void
     {
         $options = Options::default();
-        $dir = getenv(Options::DIR_ENV_NAME);
-        $configFile = $dir . '/' . Options::DEFAULT_CONFIG_NAME;
-        $stateFile = $dir . '/' . Options::DEFAULT_STATE_NAME;
-        $logFile = $dir . '/' . Options::DEFAULT_LOG_NAME;
+        
         $this->assertFalse($options->verbose);
         $this->assertFalse($options->version);
         $this->assertFalse($options->dryRun);
         $this->assertFalse($options->help);
+
+        $this->assertOptionsDefaultFiles($options);
+    }
+
+    public function test_from_arguments(): void
+    {
+        $args = new Arguments(
+            verbose: true,
+            version: true,
+            dryRun: true,
+            help: true,
+            configFile: 'config.test',
+            stateFile: 'state.test',
+            logFile: 'log.test'
+        );
+
+        $options = Options::fromArguments($args);
+
+        $this->assertSame($args->verbose, $options->verbose);
+        $this->assertSame($args->version, $options->version);
+        $this->assertSame($args->dryRun, $options->dryRun);
+        $this->assertSame($args->help, $options->help);
+
+        $this->assertOptionsSameFiles(
+            $options,
+            $args->configFile,
+            $args->stateFile,
+            $args->logFile
+        );
+    }
+
+    public function test_from_arguments_with_default_files(): void
+    {
+        $args = new Arguments(
+            verbose: true,
+            version: true,
+            dryRun: true,
+            help: true,
+            configFile: null,
+            stateFile: null,
+            logFile: null
+        );
+
+        $options = Options::fromArguments($args);
+        $this->assertOptionsDefaultFiles($options);
+    }
+
+    private function assertOptionsDefaultFiles(Options $options): void
+    {
+        $dir = getenv(Options::DIR_ENV_NAME);
+        $configFile = $this->joinDirToFile($dir, Options::DEFAULT_CONFIG_NAME);
+        $stateFile = $this->joinDirToFile($dir, Options::DEFAULT_STATE_NAME);
+        $logFile = $this->joinDirToFile($dir, Options::DEFAULT_LOG_NAME);
+
+        $this->assertOptionsSameFiles($options, $configFile, $stateFile, $logFile);
+    }
+
+    private function assertOptionsSameFiles(
+        Options $options,
+        string $configFile, 
+        string $stateFile,
+        string $logFile,
+    ): void {
         $this->assertSame($configFile, $options->configFile);
         $this->assertSame($stateFile, $options->stateFile);
         $this->assertSame($logFile, $options->logFile);
+    }
+
+    private function joinDirToFile(string $dir, string $file): string
+    {
+        return $dir.'/'.$file;
     }
 }
